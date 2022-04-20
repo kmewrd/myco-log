d1f368f734b5a0117c3f247909cca1a4bb59b569
Kim Ward <kmewrd@outlook.com>
2022-04-20 11:39:17 -0500

    Begin filling out Dashboard info

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const Dashboard = ({ user }) => {
+  const sightings = user.sightings;
+  console.log(sightings);
   return (
     <div>
-      Test!
+      <section>
+        <h2>Dashboard</h2>
+        <div>
+          <h3>Region: </h3>
+          <h3>Total sightings: </h3>
+          <h3>Sightings this month: </h3>
+        </div>
+      </section>
+      <section>
+        <h2>My Sightings</h2>
+
+      </section>
     </div>
   )
 }
