16909a40f81a33b5e0fe90d3e52c1160b61fec60
Kim Ward <kmewrd@outlook.com>
2022-04-20 18:57:06 -0500

    Add hooks and method to get sightings this month

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sighting from './Sighting';
 
-const Dashboard = ({ user, sightings, getSightings }) => {
-  const mySightings = sightings.map(sighting => <Sighting key={sighting.id} id={sighting.id} date={sighting.date} location={sighting.location} notes={sighting.notes}/>)
+const Dashboard = ({ user, sightings }) => {
+  const [sightingsThisMonth, setSightingsThisMonth] = useState([]);
+  
+  const userSightings = sightings.map(sighting => <Sighting key={sighting.id} id={sighting.id} date={sighting.date} location={sighting.location} notes={sighting.notes}/>)
 
   useEffect(() => {
-    getSightings();
+    getSightingsThisMonth();
   }, [])
+
+  const getSightingsThisMonth = () => {
+    let month = new Date();
+    month = month.getMonth();
+  
+    let monthlySightings = sightings.filter(sighting => {
+      let date = new Date(sighting.date);
+      let sightingMonth = date.getMonth();
+      return sightingMonth === month;
+    });
+  
+    setSightingsThisMonth(monthlySightings);
+  }
 
   return (
     <div>
@@ -14,13 +29,13 @@ const Dashboard = ({ user, sightings, getSightings }) => {
         <h2>Dashboard</h2>
         <div>
           <h3>Region: {user.region}</h3>
-          <h3>Total sightings: </h3>
-          <h3>Sightings this month: </h3>
+          <h3>Total sightings: {sightings.length}</h3>
+          <h3>Sightings this month: {sightingsThisMonth.length}</h3>
         </div>
       </section>
       <section>
         <h2>My Sightings</h2>
-        {mySightings}
+        {userSightings}
       </section>
     </div>
   )
