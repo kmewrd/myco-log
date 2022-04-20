79695e6dd74f1b60a4eff0d9fb656e2a23f39d32
Kim Ward <kmewrd@outlook.com>
2022-04-20 16:00:57 -0500

    Finalize POST and DELETE fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,10 +8,14 @@ export const fetchRegionalFungi = region => {
   return fetch(`https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungi/${region}`).then(response => response.json())
 }
 
-// export const postSighting = sighting => {
-//   return fetch('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', {
-//     method: 'POST',
-//     headers: { 'Content-Type': 'application/json' },
-//     body: JSON.stringify(sighting)
-//   }).then(response => response.json())
-// }
+export const postSighting = sighting => {
+  return fetch('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(sighting)
+  }).then(response => response.json())
+}
+
+export const deleteSighting = id => {
+  return fetch(`http://localhost:3001/api/v1/sightings/${id}`, { method: 'DELETE' }).then(response => response.json())
+}
