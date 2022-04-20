5710539bb039fca8684cbdee762a53d51b5bf345
Kim Ward <kmewrd@outlook.com>
2022-04-20 11:30:00 -0500

    Add fetch for regional fungi and begin writing post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,15 @@ export const fetchUser = username => {
 
   return fetch(`https://unidentified-fungus-outdoors.herokuapp.com/api/v1/users/${userId}`).then(response => response.json())
 }
+
+export const fetchRegionalFungi = region => {
+  return fetch(`https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungi/${region}`).then(response => response.json())
+}
+
+// export const postSighting = sighting => {
+//   return fetch('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(sighting)
+//   }).then(response => response.json())
+// }
