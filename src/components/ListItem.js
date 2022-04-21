c46b41cd0648d9adafac1d27d8c59f4f8a6c865f
Kim Ward <kmewrd@outlook.com>
2022-04-20 20:09:26 -0500

    Add JSX elements to ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,7 +3,10 @@ import React from 'react';
 const ListItem = ({ id, name, scientificName, regions, description, characteristics, imageUrl }) => {
   return (
     <div>
-      
+      <img src={imageUrl} />
+      <h3>{name}</h3>
+      <h4>{scientificName}</h4>
+      <button>View</button>
     </div>
   )
 }
