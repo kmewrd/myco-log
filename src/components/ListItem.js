2d06922c440d44170d13c3d3df06eb97a4bc98be
Kim Ward <kmewrd@outlook.com>
2022-04-20 20:05:48 -0500

    Add ListItem component

---
+++ src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListItem = ({ id, name, scientificName, regions, description, characteristics, imageUrl }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListItem;
