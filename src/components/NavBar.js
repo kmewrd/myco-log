93dae965d53ea9c864c0a6684fa3107a3e97700d
Kim Ward <kmewrd@outlook.com>
2022-04-20 19:50:19 -0500

    Add NavBar component and connect to Dashboard

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className='main-nav'>
+        <li>Explore</li>
+        <li>Dashboard</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
