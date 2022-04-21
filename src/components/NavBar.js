f696192b92e9f462ff1fbf2fe7912a40c23a495b
Kim Ward <kmewrd@outlook.com>
2022-04-20 19:52:19 -0500

    Change list items to buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,8 @@ import React from 'react';
 const NavBar = () => {
   return (
     <nav>
-      <ul className='main-nav'>
-        <li>Explore</li>
-        <li>Dashboard</li>
-      </ul>
+      <button>Explore</button>
+      <button>Dashboard</button>
     </nav>
   )
 }
