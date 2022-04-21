cd5b46caa249c4c8604413484fe4a882867dac17
Kim Ward <kmewrd@outlook.com>
2022-04-21 16:33:08 -0500

    Add onChange handler for inputs

--- src/components/SightingForm/SightingForm.js
+++ src/components/SightingForm/SightingForm.js
@@ -11,13 +11,17 @@ class SightingForm extends Component {
     }
   }
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
   render() {
     return (
       <form>
         <button>Close</button>
-        <input type='text' name='date' value={this.state.date} />
-        <input type='text' name='location' value={this.state.location} />
-        <input type='text' name='notes' value={this.state.notes} />
+        <input type='text' name='date' value={this.state.date} onChange={e => this.handleChange(e)} />
+        <input type='text' name='location' value={this.state.location} onChange={e => this.handleChange(e)} />
+        <input type='text' name='notes' value={this.state.notes} onChange={e => this.handleChange(e)} />
         <button>SUBMIT</button>
       </form>
     )
