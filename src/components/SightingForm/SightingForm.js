405255e667a6fcf9eb05d145a4f91a91acef23a6
Kim Ward <kmewrd@outlook.com>
2022-04-21 16:25:26 -0500

    Add SightingForm component

---
+++ src/components/SightingForm/SightingForm.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import './SightingForm.css';
+
+class SightingForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <form>
+
+      </form>
+    )
+  }
+}
+
+export default SightingForm;
