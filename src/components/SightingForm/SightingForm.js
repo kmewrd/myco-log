3f5bdf5a83996d385057ae69ce4055a827668ecd
Kim Ward <kmewrd@outlook.com>
2022-04-21 16:28:38 -0500

    Add input fields and link to form state

--- src/components/SightingForm/SightingForm.js
+++ src/components/SightingForm/SightingForm.js
@@ -5,14 +5,20 @@ class SightingForm extends Component {
   constructor() {
     super();
     this.state = {
-      
+      date: '',
+      location: '',
+      notes: ''
     }
   }
 
   render() {
     return (
       <form>
-
+        <button>Close</button>
+        <input type='text' name='date' value={this.state.date} />
+        <input type='text' name='location' value={this.state.location} />
+        <input type='text' name='notes' value={this.state.notes} />
+        <button>SUBMIT</button>
       </form>
     )
   }
