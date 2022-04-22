7c96fefdf794175929a94965e07f0c668f3fcb05
Kim Ward <kmewrd@outlook.com>
2022-04-22 17:06:06 -0500

    Refactor fetchSightings and exports

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,31 +1,33 @@
 const url = 'https://unidentified-fungus-outdoors.herokuapp.com/api/v1';
 
-export const fetchUser = username => {
+const fetchUser = username => {
   const userId = username.split('mycophile').join('');
 
   return fetch(`${url}/users/${userId}`).then(response => response.json())
 }
 
-export const fetchSightings = () => {
-  return fetch('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings').then(response => response.json())
+const fetchSightings = () => {
+  return fetch(`${url}/sightings`).then(response => response.json())
 }
 
-export const fetchRegionalFungi = region => {
+const fetchRegionalFungi = region => {
   return fetch(`${url}/fungi/${region}`).then(response => response.json())
 }
 
-export const fetchFungus = id => {
+const fetchFungus = id => {
   return fetch(`${url}/fungus/${id}`).then(response => response.json())
 }
 
-export const postSighting = sighting => {
-  return fetch('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', {
+const postSighting = sighting => {
+  return fetch(`${url}/sightings`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(sighting)
   }).then(response => response.json())
 }
 
-export const deleteSighting = id => {
+const deleteSighting = id => {
   return fetch(`${url}/sightings/${id}`, { method: 'DELETE' }).then(response => response.json())
 }
+
+export { fetchUser, fetchSightings, fetchRegionalFungi, fetchFungus, postSighting, deleteSighting };
