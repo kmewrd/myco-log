26517c889267fa0bb99c5e76f356f2e7efa946e8
Kim Ward <kmewrd@outlook.com>
2022-04-23 12:05:52 -0500

    Consolidate some font and inputs styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import './SearchBar.scss';
 
 const SearchBar = ({ search }) => {
   const [searchField, setSearchField] = useState('');
