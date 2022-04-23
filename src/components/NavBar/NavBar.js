7e2d467168374ed636c6b0c237d7dbf6dc30518c
Kim Ward <kmewrd@outlook.com>
2022-04-23 13:16:31 -0500

    Change order of NavLinks

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,11 +5,11 @@ import './NavBar.scss';
 const NavBar = () => {
   return (
     <nav className='main-nav'>
-      <NavLink to='/explore' className={isActive => 'nav-link' + (isActive ? ' selected' : '')}>
-        Explore
-      </NavLink>
       <NavLink to='/dashboard' className={isActive => 'nav-link' + (isActive ? ' selected' : '')}>
         Dashboard
+      </NavLink>
+      <NavLink to='/explore' className={isActive => 'nav-link' + (isActive ? ' selected' : '')}>
+        Explore
       </NavLink>
     </nav>
   )
