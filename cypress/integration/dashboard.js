dc8f636dae9557efa0410a906376443684986acc
Kim Ward <kmewrd@outlook.com>
2022-04-23 18:53:06 -0500

    Add beforeEach for dashboard and write first test

--- cypress/integration/dashboard.js
+++ cypress/integration/dashboard.js
@@ -1,3 +1,32 @@
 describe('Dashboard view', () => {
-  
+  beforeEach(() => {
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/users/5044', { fixture: 'user.json' }).as('user-data')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', { fixture: 'sightings.json' }).as('all-sightings')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/1', { fixture: 'fungus-1.json' }).as('fungus-1')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/34', { fixture: 'fungus-34.json' }).as('fungus-34')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/40', { fixture: 'fungus-40.json' }).as('fungus-40')
+
+    cy.visit('http://localhost:3000')
+      .get('input:first')
+      .type('mycophile5044')
+      .get('input:last')
+      .type('fungi')
+      .get('button')
+      .click()
+  })
+
+  it('should contain a navigation bar', () => {
+    cy.get('nav')
+      .should('be.visible')
+      .and('contain', 'Dashboard')
+      .and('contain', 'Explore')
+  })
+
+  // it('should contain the user\'s region and sighting stats', () => {
+
+  // })
 })
