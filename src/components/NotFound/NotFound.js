c1d1d3aa2647e8d6c1f6890c79ee3eca64231a57
Kim Ward <kmewrd@outlook.com>
2022-04-24 12:09:44 -0500

    Center 404 error message

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import './NotFound.scss';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <h2>404</h2>
       <p>That page was not found! Please return to <Link to='/'>home</Link>.</p>
     </div>
