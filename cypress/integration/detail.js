e856027274e80ac851e0e5725c93ab074e034d3f
Kim Ward <kmewrd@outlook.com>
2022-04-23 19:39:56 -0500

    Add beforeEach to detail file and test for url change

---
+++ cypress/integration/detail.js
@@ -0,0 +1,32 @@
+describe('Detail page', () => {
+  beforeEach(() => {
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/users/5044', { fixture: 'user.json' }).as('user-data')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/sightings', { fixture: 'sightings.json' }).as('all-sightings')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/1', { fixture: 'fungus-1.json' }).as('fungus-1')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/34', { fixture: 'fungus-34.json' }).as('fungus-34')
+
+    cy.intercept('https://unidentified-fungus-outdoors.herokuapp.com/api/v1/fungus/40', { fixture: 'fungus-40.json' }).as('fungus-40')
+
+    cy.visit('http://localhost:3000')
+      .get('input:first')
+      .type('mycophile5044')
+      .get('input:last')
+      .type('fungi')
+      .get('button')
+      .click()
+      .get('nav a:last')
+      .click()
+      .get('div[id="1"] a')
+      .click()
+  })
+
+  it('should update the url when a user clicks on the detail page link', () => {
+    cy.get('div[id="1"] a')
+      .click()
+      .url()
+      .should('eq', 'http://localhost:3000/explore/1')
+  })
+})
