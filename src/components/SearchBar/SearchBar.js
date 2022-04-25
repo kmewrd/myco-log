72af729e06bc30ff46ad9e81515536e16b9ab50c
Kim Ward <kmewrd@outlook.com>
2022-04-25 18:21:10 -0500

    Fix bug with GO button logging user out on enter

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,13 +5,16 @@ import './SearchBar.scss';
 const SearchBar = ({ search }) => {
   const [searchField, setSearchField] = useState('');
 
+  const submitSearch = e => {
+    e.preventDefault();
+    search(searchField)
+    setSearchField('');
+  }
+
   return (
     <form className='search-bar'>
       <input type='text' placeholder='Search...' value={searchField} onChange={e => setSearchField(e.target.value)} />
-      <button className='search-button' type='button' onClick={() => {
-        search(searchField)
-        setSearchField('');
-      }}>GO</button>
+      <button className='search-button' type='submit' onClick={e => submitSearch(e)}>GO</button>
     </form>
   )
 }
